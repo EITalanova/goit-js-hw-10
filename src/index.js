fe5fd2a2407d5f46901192bdd347fd7ea4a6bf9b
EITalanova <elenabelichenko77@gmail.com>
2023-02-01 13:36:36 -0800

    Added fetchCountries function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+function fetchCountries() {
+    return fetch('https://restcountries.com/v3.1/name/ukraine').then(
+        response => response.json())
+        .then(console.log());
+}
+fetchCountries();
