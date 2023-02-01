3cf1d2f633f48d3eb5717bd81fcce27e6c9fc2ff
EITalanova <98708004+EITalanova@users.noreply.github.com>
2023-02-01 18:00:55 +0000

    Deploying to gh-pages from @ EITalanova/goit-js-hw-10@1491495fe6b755786bbf9bd7340dd69d8d3abbb7 🚀

--- index.7df1b875.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((e=>e.json())).then(console.log());
-//# sourceMappingURL=index.7df1b875.js.map
--- index.ffebc67f.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v3.1/name/ukraine").then((function(n){return n.json()})).then(console.log());
-//# sourceMappingURL=index.ffebc67f.js.map
